Fix bug in covariance calculation

custom_gmm.py
@@ -28,9 +28,9 @@ class GMM():
             covariances_init[j] = np.linalg.inv(precision_mat)
 
         # Following are model parameters that will be set once it is fitted
-        self.weights_ = weights_init    # The weights of each mixture component
-        self.means_ = means_init  # The mean of each mixture components
-        self.covariances_ = covariances_init    # The covariance of each mixture component
+        self.weights_ = np.array(weights_init)    # The weights of each mixture component
+        self.means_ = np.array(means_init)  # The mean of each mixture components
+        self.covariances_ = np.array(covariances_init)    # The covariance of each mixture component
 
 
     def fit(self, X):
@@ -46,6 +46,8 @@ class GMM():
         # We initialize the 'w' matrix here and pass to e_step
         # This avoids unnecessary memory allocs/deallocs (that may occur if w were to be created and as destroyed in _e_step() at each iteration)
         w = np.zeros([num_samples, self.n_components])  # MxK matrix
+
+        print("Initial values: weights={}, means={}, covs={}".format(self.weights_, self.means_, self.covariances_))
 
         for n_iter in range(1, self.max_iter + 1):
             print("\nEM iteration={}".format(n_iter))
@@ -131,9 +133,9 @@ class GMM():
         data_dimension = X.shape[1]
 
         for j, weight in enumerate(self.weights_):
+            # First set new weights and means
             w_sum = 0
             mean_numerator = np.zeros([data_dimension])
-            cov_numerator_total = np.zeros([data_dimension, data_dimension])
 
             for i, sample in enumerate(X):
                 w_sum = w_sum + w[i][j]
@@ -142,8 +144,16 @@ class GMM():
             self.weights_[j] = w_sum / num_samples
             self.means_[j] = mean_numerator / w_sum
 
+
+            # Then set new covariance matrix
+
+            cov_numerator_total = np.zeros([data_dimension, data_dimension])
+
             for i, sample in enumerate(X):
-                cov_numerator = w[i][j] * np.matmul(sample - self.means_[j], np.transpose(sample - self.means_[j]))
+                diff = sample - self.means_[j]
+                diff_reshaped = np.reshape(diff, [len(diff), 1])
+                diff_transpose = np.transpose(diff_reshaped)
+                cov_numerator = w[i][j] * np.matmul(diff_reshaped, diff_transpose)
                 cov_numerator_total = cov_numerator_total + cov_numerator
 
             # print("j={}, w_sum={}, cov_numerator={}".format(j, w_sum, cov_numerator_total))

test_scikit_gmm.py
@@ -79,7 +79,8 @@ np.random.seed(0)
 
 # generate spherical data centered on (20, 20)
 mean1 = [10, 10]
-shifted_gaussian = np.random.randn(n_samples, 2) + np.array(mean1)
+cov1 = [[1, 0], [0, 1]]
+shifted_gaussian = np.random.multivariate_normal(mean1, cov1, n_samples)
 class1_target = np.zeros((n_samples, 1))
 
 # generate zero centered stretched Gaussian data
@@ -95,18 +96,20 @@ X_train = np.vstack([shifted_gaussian, stretched_gaussian])
 y_train = np.vstack([class1_target, class2_target])
 
 # Initial values for model parameters
-weights_init = [0.4, 0.6]
-# means_init = [[11, 11], [1, 1]]
-means_init = [[10, 10], [0, 0]]
-# cov_init = [[[1, 0.5], [0.5, 1]], [[1, 0.5], [0.5, 1]]]
-cov_init = [[[1, 0.5], [0.5, 1]], cov2]
-precisions_init = np.linalg.inv(cov_init)
+weights_init = [0.5, 0.5]
+# means_init = [mean1, mean2]
+means_init = [[15, 15], [-5, -5]]
+precisions_init = [np.linalg.inv(cov1), np.linalg.inv(cov2)]
 #print(precisions_init)
 
 # fit a Gaussian Mixture Model with two components
 gmm = GaussianMixture(n_components=2, covariance_type='full', max_iter=50,
                       weights_init=weights_init, means_init=means_init, precisions_init=precisions_init)
 gmm.fit(X_train)
+print("Trained GMM model parameters")
+print("weights={}".format(gmm.weights_))
+print("means={}".format(gmm.means_))
+print("covariances={}".format(gmm.covariances_))
 
 # print some model selection metrics
 bic = gmm.bic(X_train)    # Bayesian Information Criterion