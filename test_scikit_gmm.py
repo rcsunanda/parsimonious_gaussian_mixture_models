fe95c88680e2baecf68061daf4dd1b23260072b2
Sunanda <rcsunanda@gmail.com>
2018-11-02 00:50:30 -0400

    Measure clustering performance with Rand index and Adjusted Rand index

--- test_scikit_gmm.py
+++ test_scikit_gmm.py
@@ -1,14 +1,34 @@
 import numpy as np
 from sklearn import mixture
+from sklearn import metrics
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import matplotlib as mpl
 import itertools
 
+def compute_rand_index(cluster1, cluster2):
+    n = len(cluster1)
+    total_pairs = n * (n-1) / 2
+    pairs_in_agreement = 0
+    # total_iter = 0
+
+    for i in range(n):
+        for j in range(i+1, n):
+            # total_iter = total_iter + 1
+            if (cluster1[i] == cluster1 [j] and cluster2[i] == cluster2[j]) or  \
+                (cluster1[i] != cluster1 [j] and cluster2[i] != cluster2[j]):
+                pairs_in_agreement = pairs_in_agreement + 1
+
+    rand_index = pairs_in_agreement / total_pairs
+    # print("total_pairs={}, total_iter={}, pairs_in_agreement={}, rand_index={}".
+    #       format(total_pairs, total_iter, pairs_in_agreement, rand_index))
+    return rand_index
+
+
 def make_ellipses(gmm, ax, X_data):
     color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue', 'darkorange'])
-    scatter_markers = itertools.cycle(['o', 'X', 'v', '^'])
+    scatter_markers = itertools.cycle(['o', '+', 'v', '^'])
     for n, (mean, cov, color, marker) in enumerate(zip(gmm.means_, gmm.covariances_,
                                                        color_iter, scatter_markers)):
         covariances = gmm.covariances_[n][:2, :2]
@@ -24,7 +44,8 @@ def make_ellipses(gmm, ax, X_data):
         ell.set_clip_box(ax.bbox)
         ell.set_alpha(0.5)
         ax.add_artist(ell)
-        plt.scatter(X_data[:, 0], X_data[:, 1], s=.8, marker=marker)
+        # print(marker)
+        plt.scatter(X_data[:, 0], X_data[:, 1], s=1, marker=marker)
 
 
 def plot_score_contours(gmm, ax, X_data):
@@ -47,14 +68,19 @@ n_samples = 300  # was 300 originally
 np.random.seed(0)
 
 # generate spherical data centered on (20, 20)
-shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
+mean1 = [10, 10]
+shifted_gaussian = np.random.randn(n_samples, 2) + np.array(mean1)
+class1_target = np.zeros((n_samples, 1))
 
 # generate zero centered stretched Gaussian data
+mean2 = [0, 0]
 C = np.array([[0., -0.7], [3.5, .7]])
-stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
+stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C) + mean2
+class2_target = np.ones((n_samples, 1))
 
 # concatenate the two datasets into the final training set
 X_train = np.vstack([shifted_gaussian, stretched_gaussian])
+y_train = np.vstack([class1_target, class2_target])
 
 # fit a Gaussian Mixture Model with two components
 gmm = mixture.GaussianMixture(n_components=2, covariance_type='full')
@@ -64,6 +90,30 @@ gmm.fit(X_train)
 bic = gmm.bic(X_train)    # Bayesian Information Criterion
 aic = gmm.aic(X_train)    # Akaike Information Criterion
 print("Model selection metrics (the lower the better): BIC={}, AIC={}".format(bic, aic))
+
+# predict on training data and measure training Rand index and adjusted Rand index
+pred_train = gmm.predict(X_train)
+
+rand_index = compute_rand_index(y_train, pred_train)
+adjusted_rand_index = metrics.adjusted_rand_score(y_train.flatten(), pred_train.flatten())
+print("Training data: Rand index={}, Adjusted Rand index={}".format(rand_index, adjusted_rand_index))
+
+# Generate some test data
+test_shifted_gaussian = np.random.randn(n_samples, 2) + np.array(mean1)
+test_class1_target = np.zeros((n_samples, 1))
+test_stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C) + mean2
+test_class2_target = np.ones((n_samples, 1))
+# concatenate the two datasets into the final test set
+X_test = np.vstack([test_shifted_gaussian, test_stretched_gaussian])
+y_test = np.vstack([test_class1_target, test_class2_target])
+
+
+# predict on test data and measure training Rand index and adjusted Rand index
+pred_test = gmm.predict(X_test)
+
+test_rand_index = compute_rand_index(y_test, pred_test)
+test_adjusted_rand_index = metrics.adjusted_rand_score(y_test.flatten(), pred_test.flatten())
+print("Test data: Rand index={}, Adjusted Rand index={}".format(test_rand_index, test_adjusted_rand_index))
 
 
 # display predicted scores by the model as a contour plot
@@ -75,8 +125,8 @@ print("Model selection metrics (the lower the better): BIC={}, AIC={}".format(bi
 # Plot an ellipse for each Gaussian component
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_xlim([-10, 25])
-ax.set_ylim([-5, 25])
+ax.set_xlim([-10, 15])
+ax.set_ylim([-5, 15])
 make_ellipses(gmm, ax, X_train)
 
 plt.show()
